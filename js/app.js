5c4fccbda47f8ca2882f1f5b4a4b44270190f24d
rishabh anand <rish@rishabhs-MacBook-Pro.local>
2022-02-28 14:22:55 +0800

    Add links to profile cards

--- js/app.js
+++ js/app.js
@@ -5,131 +5,161 @@ document.addEventListener('DOMContentLoaded', () => {
         "Presidency": [
             {
                 "name": "Raveen Prabhu",
-                "role": "President"
+                "role": "President",
+                "url": "https://www.linkedin.com/in/raveenprabhu/"
             },
             {
                 "name": "Chen Hsiao Ting",
-                "role": "General Secretary"
+                "role": "General Secretary",
+                "url": "https://www.linkedin.com/in/chenhsiaoting/"
             },
             {
                 "name": "Jenny Jian Jie",
-                "role": "Assistant General Secretary"
+                "role": "Assistant General Secretary",
+                "url": "https://www.linkedin.com/in/jenny-jian-jie/"
             },
             {
                 "name": "Rishabh Anand",
-                "role": "Tech Lead"
+                "role": "Tech Lead",
+                "url": "http://rish-16.github.io"
             },
             {
                 "name": "Megan Wee",
-                "role": "Assistant Tech Lead"
+                "role": "Assistant Tech Lead",
+                "url": "https://www.linkedin.com/in/megan-wee-rui-en/"
             },
             {
                 "name": "Tan Kian Lin",
-                "role": "Finance Secretary"
+                "role": "Finance Secretary",
+                "url": "https://www.linkedin.com/in/kianlintan"
             },
             {
                 "name": "Sophie Ng",
-                "role": "Assistant Finance Secretary"
+                "role": "Assistant Finance Secretary",
+                "url": "https://www.linkedin.com/in/sophiieng"
             }
         ],
         "External Relations": [
             {
                 "name": "Abhishek Jain ",
-                "role": "Vice President"
+                "role": "Vice President",
+                "url": "https://abhishekjainz.me/"
             },
             {
                 "name": "Xiang Qingyi",
-                "role": "Director, Publicity"
+                "role": "Director, Publicity",
+                "url": "https://www.linkedin.com/in/qingyi-xiang/"
             },
             {
                 "name": "Alyssa Savier",
-                "role": "Deputy Director, Publicity (Graphics & Content)"
+                "role": "Deputy Director, Publicity (Graphics & Content)",
+                "url": "https://sg.linkedin.com/in/alyssa-savier-b83090160"
             },
             {
                 "name": "Yap Teng Chiong",
-                "role": "Deputy Director, Publicity (Photos & Video)"
+                "role": "Deputy Director, Publicity (Photos & Video)",
+                "url": ""
             },
             {
                 "name": "Lo Zhao Wei",
-                "role": "Director, Marketing"
+                "role": "Director, Marketing",
+                "url": "https://www.linkedin.com/in/zhaoweilo/"
             },
             {
                 "name": "Shen Yu Chen",
-                "role": "Deputy Director, Marketing (Merchandise)"
+                "role": "Deputy Director, Marketing (Merchandise)",
+                "url": ""
             },
             {
                 "name": "Lin Jiayong",
-                "role": "Deputy Director, Marketing (Sponsorship)"
+                "role": "Deputy Director, Marketing (Sponsorship)",
+                "url": "https://www.linkedin.com/in/jaywhy0309"
             }
         ],
         "Internal Relations": [
             {
                 "name": "Roy Chua",
-                "role": "Vice President"
+                "role": "Vice President",
+                "url": "https://roystal.github.io/"
             },
             {
                 "name": "Xiong Jingya",
-                "role": "Director, Welfare"
+                "role": "Director, Welfare",
+                "url": "https://www.linkedin.com/in/jingya-xiong-5133a6202/"
             },
             {
                 "name": "Bryan Ong",
-                "role": "Deputy Director, Welfare"
+                "role": "Deputy Director, Welfare",
+                "url": "https://www.linkedin.com/in/bryan-ong-wx"
             },
             {
                 "name": "L Kaushik Rangaraj",
-                "role": "Director, Sports"
+                "role": "Director, Sports",
+                "url": "https://linktr.ee/KaushikR"
             },
             {
                 "name": "Denise Lam",
-                "role": "Deputy Director, Sports"
+                "role": "Deputy Director, Sports",
+                "url": ""
             } 
         ],
         "Student Development": [
             {
                 "name": "Arnav Gupta",
-                "role": "Vice President"
+                "role": "Vice President",
+                "url": "https://linktr.ee/arnavg"
             },
             {
                 "name": "David Limantara",
-                "role": "Director, Academic Liaison"
+                "role": "Director, Academic Liaison",
+                "url": "https://www.linkedin.com/in/david-limantara"
             },
             {
                 "name": "Joel Toh",
-                "role": "Deputy Director, Academic Liaison"
+                "role": "Deputy Director, Academic Liaison",
+                "url": "https://www.linkedin.com/in/joel-toh/"
             },
             {
                 "name": "Cheang Xue Ting",
-                "role": "Director, Community Service"
+                "role": "Director, Community Service",
+                "url": "http://linkedin.com/in/cheangxueting"
             },
             {
                 "name": "Wei Yangken",
-                "role": "Deputy Director, Community Service"
+                "role": "Deputy Director, Community Service",
+                "url": "https://yangkenw.netlify.app/"
             }
         ],
         "Student Life": [
             {
                 "name": "Dao Minh Hai",
-                "role": "Vice President"
+                "role": "Vice President",
+                "url": ""
             },
             {
                 "name": "Tricia Goh",
-                "role": "Director, Freshman Orientation Projects"
+                "role": "Director, Freshman Orientation Projects",
+                "url": "https://www.linkedin.com/in/tricixg/"
             },
             {
                 "name": "Summer Thia",
-                "role": "Deputy Director, Freshman Orientation Projects"
+                "role": "Deputy Director, Freshman Orientation Projects",
+                "url": "http://www.linkedin.com/in/summerthia"
             },
             {
                 "name": "Andrea Loh",
-                "role": "Deputy Director, Freshman Orientation Projects"
+                "role": "Deputy Director, Freshman Orientation Projects",
+                "url": "https://www.linkedin.com/in/andrealyh/"
             },
             {
                 "name": "Merrick Neo",
-                "role": "Director, Student Relations"
+                "role": "Director, Student Relations",
+                "url": "https://www.linkedin.com/in/merrick-neo-b66bb4201"
             },
             {
                 "name": "Low Zan Hao",
-                "role": "Deputy Director, Student Relations"
+                "role": "Deputy Director, Student Relations",
+                "url": "https://www.linkedin.com/in/zan-hao-low-1774b7207/"
             }
         ]
     }
@@ -148,7 +178,7 @@ document.addEventListener('DOMContentLoaded', () => {
         "Student Life": stdlifeBox
     }
 
-    function createCard(container, name, position) {
+    function createCard(container, name, position, url) {
         /*
             <div class="wing-card">
                 <!-- <img src="" class="wing-pic"> -->
@@ -194,6 +224,20 @@ document.addEventListener('DOMContentLoaded', () => {
         card.appendChild(cardImgCont)
         card.appendChild(details)
 
+        if (url.length > 0) {
+            card.addEventListener("click", () => {
+                window.open(url, '_blank');
+            })
+
+            cardImage.addEventListener("click", () => {
+                window.open(url, '_blank');
+            })
+
+            details.addEventListener("click", () => {
+                window.open(url, '_blank');
+            })
+        }
+
         container.append(card)
     }
 
@@ -202,7 +246,8 @@ document.addEventListener('DOMContentLoaded', () => {
         for (var i =0; i < members.length; i++) {
             var name = members[i]["name"]
             var role = members[i]["role"]
-            createCard(loc, name, role)
+            var url = members[i]["url"]
+            createCard(loc, name, role, url)
         }
     }
 })
